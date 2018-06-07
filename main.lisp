f589019fd2dc0a477de52931bb79bd1d21b8793f
Elad Bogomolny <eladbo@wix.com>
2018-06-07 14:49:04 -0400

    first load file, save-file and diff-2-files implementation

---
+++ main.lisp
@@ -0,0 +1,32 @@
+(defvar *welcome-message*  (format t "Hello, my name is ~a" "Nick"))
+(with-open-file 
+    (out "goldenfile.txt"
+         :direction :output
+         :if-exists :supersede)
+    (WITH-STANDARD-IO-SYNTAX 
+        (format out "hello, world!")))
+ 
+
+(defun save-file (filename txt)
+  (with-open-file (out filename
+                   :direction :output
+                   :if-exists :supersede)
+    (with-standard-io-syntax
+      (print txt out))))
+
+(defun load-file-content (file)
+  (let ((result ""))
+    (with-open-file (str file :direction :input)
+      (do ((line (read-line str nil 'eof)
+                 (read-line str nil 'eof)))
+          ((eql line 'eof) result)
+        (setf result (format nil "~a~%~a" result line))))))
+   
+
+(defun diff-2-files (gld-file-name test-file-name)
+  (let ((gld-file (load-file-content gld-file-name))
+        (test-file (load-file-content test-file-name)))
+    (equal gld-file test-file)))
+
+(diff-2-files "test.txt" "test.txt")    
+  
